e4b3f653befcf8506e87166816126097bfb3b980
Valentin Litvak <contact@vallitvak.com>
2013-10-05 18:09:52 -0500

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DepaulMitHacks::Application.config.secret_token = '78555a8d4757ace1c00ac3ddfb57bf6fe8a23c2759b342a8b447b5e66055202e6ffb3904fb1507a89e9e6ecf62a6320432c4845d1a74a8531cd4a8d33e6913d1'
