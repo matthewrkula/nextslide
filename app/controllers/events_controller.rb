e85eff1fe6785de1f1c311a4f889eed6094c352f
Valentin Litvak <contact@vallitvak.com>
2013-10-06 01:59:59 -0500

    fixed the image url thing in json

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,10 +4,10 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @events }
-    end
+    #respond_to do |format|
+      #format.html # index.html.erb
+      #format.json { render json: @events }
+    #end
   end
 
   # GET /events/1
