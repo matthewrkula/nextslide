7359e179bd2a28ed263fad13974495c6983ca147
Valentin Litvak <contact@vallitvak.com>
2013-10-06 05:02:21 -0500

    redirecting to event instead of event slideshows url

--- app/controllers/slideshows_controller.rb
+++ app/controllers/slideshows_controller.rb
@@ -44,9 +44,6 @@ class SlideshowsController < ApplicationController
       end
       response_object = RestClient.get("http://api.shabz.co/hackathon/get_page_count.php?url=http://docs.google.com/gview?url=#{url}&embedded=true&chrome=false")
       num_pages = JSON.parse(response_object.body)['num_pages']
-
-      #binding.pry
-
       @slideshow.slide_num = num_pages
       @slideshow.save
       i = 0
@@ -54,26 +51,10 @@ class SlideshowsController < ApplicationController
         @slideshow.slides.create!({ slide_number: i + 1 })
         i = i + 1
       end
-      redirect_to event_slideshows_url(@event), notice: 'Created a slideshow successfully.'
+      redirect_to event_url(@event), notice: 'Created a slideshow successfully.'
     else
       render :new
     end
-
-		# url = params[:url]
-		# event_id = @event.id
-		# name = params[:name]
-
-
-		#ss = Slideshow.new({url: url, slide_num: num_pages, event_id: event_id, name: name})
-		#ss.save!
-
-
-    #@slideshow = ss
-
-
-		#respond_to do |format|
-			#format.js
-		#end
 	end
 
 private
