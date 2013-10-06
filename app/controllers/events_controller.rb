99b73bc51a2b482a18abb2e79db5b32d5f35b477
Valentin Litvak <contact@vallitvak.com>
2013-10-06 02:00:21 -0500

    took out the response stuff, because we are render json in the views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,11 +3,6 @@ class EventsController < ApplicationController
   # GET /events.json
   def index
     @events = Event.all
-
-    #respond_to do |format|
-      #format.html # index.html.erb
-      #format.json { render json: @events }
-    #end
   end
 
   # GET /events/1
