e8937ccc3c00b8304eb8ebabbce2956711a15dce
Valentin Litvak <contact@vallitvak.com>
2013-10-11 19:55:07 -0500

    rolling out changes to heroku

--- app/assets/javascripts/dashboard/base.js
+++ app/assets/javascripts/dashboard/base.js
@@ -1,21 +1,40 @@
-window.onload = function() {
-  console.log('worked!');
-  var slideshowsList = document.getElementById('slideshows-list'),
-      slideshowsListChildren = slideshowsList.children,
-      slideshowListItems = [],
-      i;
+(function() {
+  window.onload = function() {
+    var self = this,
+        slideshowsList = document.getElementById('slideshows-list'),
+        slideshowsListChildren = slideshowsList.children,
+        slideshowListItems = [],
+        i, j;
 
-  for (i = 0; i < slideshowsList.children.length; i++) {
-    if (slideshowsListChildren[i].className == 'slideshow-item')
-      slideshowListItems.push(slideshowsListChildren[i]);
-  }
+    for (i = 0; i < slideshowsList.children.length; i++) {
+      if (slideshowsListChildren[i].className.match(/slideshow-item/g))
+        slideshowListItems.push(slideshowsListChildren[i]);
+    }
 
-  for (i = 0; i < slideshowListItems.length; i++) {
-    (function(_i){
-      slideshowListItems[_i].onmouseover = function(e) {
-        slideshowListItems[_i].
-        console.log(slideshowListItems[_i]);
-      }
-    })(i);
-  }
-};
+    for (i = 0; i < slideshowListItems.length; i++) {
+      (function(_i) {
+        var tempUrl, tempChildren, tempEditLink;
+
+        tempChildren = slideshowListItems[_i].children;
+        for (j = 0; j < slideshowListItems[_i].children.length; j++) {
+          if (slideshowListItems[_i].children[j].className.match(/slideshow-url/g))
+            tempUrl = slideshowListItems[_i].children[j].innerHTML;
+          if (slideshowListItems[_i].children[j].className.match(/slideshow-item-edit-link/g))
+            tempEditLink = slideshowListItems[_i].children[j];
+        }
+
+        slideshowListItems[_i].onclick = function(e) {
+          window.location.href = tempUrl;
+        };
+
+        slideshowListItems[_i].onmouseover = function(e) {
+          tempEditLink.style.display = 'block';
+        };
+
+        slideshowListItems[_i].onmouseout = function(e) {
+          tempEditLink.style.display = 'none';
+        };
+      })(i);
+    }
+  };
+})();
